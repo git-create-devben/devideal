0d0007f1544519eb601cf90c338cda1d9ed6a906
git-create-devben <benlad636@gmail.com>
2024-01-29 02:33:23 -0800

    added background pattern

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export function Logo(){
     return (
         <React.Fragment>
-            
+            <div className="">
+                D <b>.</b>
+            </div>
         </React.Fragment>
     )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function navbar(){
     return (
         <React.Fragment>
-             <nav className="p-2 md:flex md:justify-center">
+             {/* <nav className="p-2 md:flex md:justify-center">
                <div className="flex h-16">
                   
                </div>
-             </nav>
+             </nav> */}
         </React.Fragment>
     )
 }
