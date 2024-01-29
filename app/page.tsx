1026080df975c2855adef37d63665ab25763c9c2
git-create-devben <benlad636@gmail.com>
2024-01-29 02:11:03 -0800

    add shadcdn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-
+import Navbar from "@/components/navbar/navbar";
 export default function Home() {
   return (
     <main className="container">
+      <Navbar/>
       <h1 className="text-2xl text-red-400 text-balance">Hello</h1>
     </main>
   );
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function navbar(){
     return (
         <React.Fragment>
-            
+             <nav className="p-2 md:flex md:justify-center">
+               <div className="flex h-16">
+                  
+               </div>
+             </nav>
         </React.Fragment>
     )
 }
