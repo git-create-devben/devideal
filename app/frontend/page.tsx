6034953aac3cd31ac27d2bcdd6024d6c57046b43
git-create-devben <benlad636@gmail.com>
2024-02-01 03:56:31 -0800

    added react flow

--- app/frontend/page.tsx
+++ app/frontend/page.tsx
@@ -1,11 +1,19 @@
-import { auth, currentUser } from "@clerk/nextjs";
+import React from "react";
+import {ReactFlow} from "reactflow";
 
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 const frontend = () => {
-  const { userId } = auth();
-  const user = currentUser();
-  return <>{!user && <h1 className="text-red-400">Hello front-end no authorize</h1>}
+  <>
     <main>
-        <h1 className="text-blue-400">Hello frontend</h1>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      </div>
     </main>
   </>;
 };
