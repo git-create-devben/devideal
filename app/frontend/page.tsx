2913f8a57c09396011759de65c332a02daadad16
git-create-devben <benlad636@gmail.com>
2024-02-01 19:21:06 -0800

    update

--- app/frontend/page.tsx
+++ app/frontend/page.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-import {ReactFlow} from "reactflow";
 
-import "reactflow/dist/style.css";
+const Frontend = () => {
+    return
+    <React.Fragment>
+        <h1 className="text-2xl text-red-400">Hello Reactflow</h1>
+    </React.Fragment>
+}
 
-const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-const frontend = () => {
-  <>
-    <main>
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <ReactFlow nodes={initialNodes} edges={initialEdges} />
-      </div>
-    </main>
-  </>;
-};
-
-export default frontend;
+export default Frontend;
